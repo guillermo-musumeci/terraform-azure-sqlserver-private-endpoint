cfe74d09b35b2fd6ab62f1017346af2f95e70b7b
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2020-11-14 21:26:28 +0100

    Initial commit

--- network-output.tf
+++ network-output.tf
@@ -3,5 +3,6 @@
 ######################
 
 output "sql_private_link_endpoint_ip" {
+  description = "SQL Private Link Endpoint IP"
   value = data.azurerm_private_endpoint_connection.kopi-endpoint-connection.private_service_connection.0.private_ip_address
 }
