86cf0c8b296f97b9e8a1e42b3d8db4e5100a998a
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2020-11-14 23:24:18 +0100

    Initial commit

--- sql-server-output.tf
+++ sql-server-output.tf
@@ -6,7 +6,3 @@ output "sql_db" {
   description = "SQL Server DB and Database"
   value       = "${azurerm_sql_database.kopi-sql-db.server_name}/${azurerm_sql_database.kopi-sql-db.name}"
 }
-
-# output "sql_name" {
-#   value = azurerm_sql_database.kopi-sql-db.fully_qualified_domain_name
-# }
